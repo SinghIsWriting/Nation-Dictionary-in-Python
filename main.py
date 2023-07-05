edbd6c9692b237a90d425b25db08b17ea873a23a
Singh <sabhisheksignh343104@gmail.com>
2023-07-05 19:22:44 +0530

    comments added in main.py file

--- main.py
+++ main.py
@@ -1,59 +1,58 @@
-#!/data/data/com.termux/files/usr/bin/python3
-import colorama
+import colorama   # importing colorama python library for colorful text
 from colorama import Fore
 colorama.init(autoreset=True)
-from countryinfo import CountryInfo
+from countryinfo import CountryInfo   # using countryinfo python library
 
+# take input country name
 c = input("Enter country: ").title()
 
 country = CountryInfo(c)
-#print(dir(country))
 
-data = country.alt_spellings()
-data1 = country.capital()
-data2 = country.capital_latlng()
-data3 = country.calling_codes()
-data4 = country.area()
-data5 = country.population()
-data6 = country.borders()
-data7 = country.currencies()
-data8 = country.languages()
-data10 = country.demonym()
-data11 = country.flag()
-data12 = country.geo_json()
-data13 = country.iso()
-data14 = country.latlng()
-data15 = country.name()
-data16 = country.native_name()
-data17 = country.provinces()
-data18 = country.region()
-data19 = country.subregion()
-data20 = country.timezones()
-data21 = country.tld()
-data22 = country.translations()
-data23 = country.wiki()
+spelling= country.alt_spellings()          # spelling of country name
+capital_of_country= country.capital()      # capital of country
+capitals_lanlng= country.capital_latlng()  # latitude and longitude of country
+callingCode = country.calling_codes()      # calling code of country
+areaOfCountry = country.area()             # area of country
+populationOfCountry = country.population() # population of country
+neighbourCountries = country.borders()     # border countries of this country
+currenciesInCountry = country.currencies() # valid currencies in the country
+languageInCountry = country.languages()    # languages in the country
+residents = country.demonym()              # name natives/residents
+nationalFlag = country.flag()              # flag of country
+geography = country.geo_json()             # geography of country
+isos = country.iso()                       # Iso of country
+coordinates = country.latlng()             # latitude and longitude of country
+countryName = country.name()               # full name of country
+nativeNames = country.native_name()        # name of natives
+provincesInCountry = country.provinces()   # list of all provinces of the country
+regionOfCountry = country.region()         # region of the country
+subregionOfCountry = country.subregion()   # subregion of the country
+timezone = country.timezones()             # timezones of the country
+topLevelDomain = country.tld()             # Internet country code top-level domain
+translation = country.translations()       # translations in country
+wikiURL = country.wiki()                   # wikipedia URL of country
 
-print(f"{Fore.GREEN}Speelings:",data)
-print(f"{Fore.GREEN}Capital:",data1)
-print(f"{Fore.GREEN}Capital's lat,lon:",data2)
-print(f"{Fore.GREEN}Calling code:",data3)
-print(f"{Fore.GREEN}Area:",data4)
-print(f"{Fore.GREEN}Population:",data5)
-print(f"{Fore.GREEN}Border counteies:",data6)
-print(f"{Fore.GREEN}Currencies:",data7)
-print(f"{Fore.GREEN}Languages:",data8)
-print(f"{Fore.GREEN}Demonym:",data10)
-print(f"{Fore.GREEN}flag:",data11)
-print(f"{Fore.GREEN}Geograpy:",data12['features'][0]['id'],",",data12['features'][0]['properties']['name'],",",data12['features'][0]['geometry']['type'])
-print(f"{Fore.GREEN}Iso:",data13)
-print(f"{Fore.GREEN}Latitude, Longitude:",data14)
-print(f"{Fore.GREEN}Name:",data15)
-print(f"{Fore.GREEN}Native name:",data16)
-print(f"{Fore.GREEN}Provinces:",len(data17),"\n",data17)
-print(f"{Fore.GREEN}Region:",data18)
-print(f"{Fore.GREEN}Sub-region:",data19)
-print(f"{Fore.GREEN}Timezone:",data20)
-print(f"{Fore.GREEN}Tld:",data21)
-print(f"{Fore.GREEN}Translations:",data22)
-print(f"{Fore.GREEN}Visit wikipedia:",data23)
+print(f"{Fore.GREEN}Speelings:",spelling)
+print(f"{Fore.GREEN}Capital:",capital_of_country)
+print(f"{Fore.GREEN}Capital's lat,lon:",capitals_lanlng)
+print(f"{Fore.GREEN}Calling code:",callingCode)
+print(f"{Fore.GREEN}Area:",areaOfCountry)
+print(f"{Fore.GREEN}Population:",populationOfCountry)
+print(f"{Fore.GREEN}Border counteies:",neighbourCountries)
+print(f"{Fore.GREEN}Currencies:",currenciesInCountry)
+print(f"{Fore.GREEN}Languages:",languageInCountry)
+print(f"{Fore.GREEN}Demonym/Residents:",residents)
+print(f"{Fore.GREEN}flag:",nationalFlag)
+print(f"{Fore.GREEN}Geograpy:",geography['features'][0]['id'],",",geography['features'][0]['properties']['name'],",",geography['features'][0]['geometry']['type'])
+print(f"{Fore.GREEN}Iso:",isos)
+print(f"{Fore.GREEN}Latitude, Longitude:",coordinates)
+print(f"{Fore.GREEN}Name:",countryName)
+print(f"{Fore.GREEN}Native name:",nativeNames)
+print(f"{Fore.GREEN}Provinces:",len(provincesInCountry),"\n",provincesInCountry)
+print(f"{Fore.GREEN}Region:",regionOfCountry)
+print(f"{Fore.GREEN}Sub-region:",subregionOfCountry)
+print(f"{Fore.GREEN}Timezone:",timezone)
+print(f"{Fore.GREEN}Tld:",topLevelDomain)
+print(f"{Fore.GREEN}Translations:",translation)
+print(f"{Fore.GREEN}Visit wikipedia:",wikiURL)
 
